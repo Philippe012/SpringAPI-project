4a0b26a12ae10c66bce727a815718c6a51725b54
Philippe <157279418+Philippe012@users.noreply.github.com>
2024-03-28 22:24:58 +0200

    Update UserController.java

--- src/main/java/com/example/springapi/APi/Controller/UserController.java
+++ src/main/java/com/example/springapi/APi/Controller/UserController.java
@@ -1,2 +1,27 @@
-package com.example.springapi.APi.Controller;public class UserController {
+package com.example.springapi.APi.Controller;
+
+import com.example.springapi.APi.Controller.Model.User;
+import com.example.springapi.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+        Optional<User> userOptional = userService.getUser(id);
+        return userOptional.orElse(null);
+    }
 }
